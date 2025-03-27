35e2a085330de698c5bfedcc6328caeaa9fcd779
Greg Miller <hypurban86@gmail.com>
2025-03-26 22:41:40 -0400

    updates for commonJS

---
+++ cjs/index.cjs.js
@@ -0,0 +1,14 @@
+/*
+	index.cjs.js
+	------------
+
+	CommonJS re-exports of the frontend-safe utilities.
+*/
+const { socketRef, socketShallowRef } = require('./socketRefClient');
+const { bindRefs } = require('./bindRefs');
+
+module.exports = {
+	bindRefs,
+	socketRef,
+	socketShallowRef
+};
